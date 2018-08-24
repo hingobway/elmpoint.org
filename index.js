b4b2c68dcb54599a0a9702efee94b177db9fa82c
hingobway <roadsterfoster@gmail.com>
2018-08-24 16:28:41 -0400

    Added EJS template engine & renamed env.js

--- index.js
+++ index.js
@@ -4,16 +4,22 @@ const mongoose = require('mongoose');
 const bp = require('body-parser');
 const https = require('https');
 const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const ejs = require('ejs');
 
+//
 // ENV VARS
 const { MONGODB_URI } = process.env;
 
 const upl = multer();
 
+//
 // ROUTER INIT
 const app = express();
 const port = process.env.PORT || 8080;
 
+//
 // DB INIT
 mongoose
   .connect(
@@ -22,14 +28,34 @@ mongoose
   )
   .then(() => console.log('MongoDB connected'), err => console.log(err));
 
-// MIDDLEWARE
+//
+// MIDDLEWARE / CONFIG
+app.set('views', path.join(__dirname, '/client'));
+app.set('view engine', 'html');
+app.engine('html', ejs.__express);
+
 app.use(bp.json());
 app.use(bp.urlencoded({ extended: true }));
 
+//
 // ROUTES
 app.use('/api', require('./api'));
 
-app.use(express.static(__dirname + '/client'));
+// serve HTML
+app.use(async (req, res, next) => {
+  app.render(
+    'pages' + req.path + (req.path !== '/' ? '.html' : ''),
+    (err, page) => {
+      if (err) return next();
+      res.render('', {
+        page
+      });
+    }
+  );
+});
+
+// serve static files
+app.use(express.static(__dirname + '/public'));
 
 // LISTEN
 app.listen(port, () => console.log('Listening on ' + port));
